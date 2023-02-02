b9469383491b8892b5f45f7fc330019804b53256
Konstantin Honcharov <k.honcharov@gmail.com>
2023-02-02 12:08:01 +0300

    feat: add plans data and types

---
+++ src/app/plan-info.ts
@@ -0,0 +1,49 @@
+import { MembershipPlan, AdditionalService } from './types';
+
+export const plan: MembershipPlan[] = [
+  {
+    name: 'Arcade',
+    description: '2 months free',
+    costPerMonth: 9,
+    costPerYear: 90,
+    currency: 'USD',
+  },
+  {
+    name: 'Advanced',
+    description: '2 months free',
+    costPerMonth: 12,
+    costPerYear: 120,
+    currency: 'USD',
+  },
+  {
+    name: 'Pro',
+    description: '2 months free',
+    costPerMonth: 15,
+    costPerYear: 150,
+    currency: 'USD',
+  },
+];
+
+export const addon: AdditionalService[] = [
+  {
+    name: 'Online service',
+    description: 'Access to multiplayer games',
+    costPerMonth: 1,
+    costPerYear: 10,
+    currency: 'USD',
+  },
+  {
+    name: 'Larger storage',
+    description: 'Extra 1TB of cloud save',
+    costPerMonth: 2,
+    costPerYear: 20,
+    currency: 'USD',
+  },
+  {
+    name: 'Customizable Profile',
+    description: 'Custom theme on your profile',
+    costPerMonth: 2,
+    costPerYear: 20,
+    currency: 'USD',
+  },
+];
--- src/app/types.ts
+++ src/app/types.ts
@@ -6,8 +6,34 @@ export enum FormStep {
   STEP5,
 }
 
+export type PaymentPeriod = 'month' | 'year';
+
+export type Currency = 'USD' | 'EUR';
+
+export interface MembershipPlan {
+  name: string;
+  description: string;
+  costPerMonth: number;
+  costPerYear: number;
+  currency: Currency;
+}
+
+export interface AdditionalService {
+  name: string;
+  description: string;
+  costPerMonth: number;
+  costPerYear: number;
+  currency: Currency;
+}
+
 export interface User {
   name: string;
   email: string;
   phone: string;
+}
+
+export interface PlanDataType extends User {
+  plan: MembershipPlan;
+  paymentPeriod: PaymentPeriod;
+  addons: AdditionalService[];
 }
