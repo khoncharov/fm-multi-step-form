d145482530d6e8f148e363c5c0097d68af99ab0c
Konstantin Honcharov <k.honcharov@gmail.com>
2023-02-03 08:59:14 +0300

    fix: fix err msg

--- src/app/data/const.ts
+++ src/app/data/const.ts
@@ -1,4 +1,4 @@
 export const MSG_REQUIRED_FIELD = 'This field is required';
-export const MSG_WRONG_NAME = 'Latin letter only';
+export const MSG_WRONG_NAME = 'Latin letters only, min 3 letters';
 export const MSG_WRONG_EMAIL = 'Wrong email format';
 export const MSG_WRONG_PHONE = 'Wrong phone format';
