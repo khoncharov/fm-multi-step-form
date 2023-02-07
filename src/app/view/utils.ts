3a9e21ed273804bbe2f1f4480898da2dcd0a0478
Konstantin Honcharov <k.honcharov@gmail.com>
2023-02-07 04:26:10 +0300

    fix: remove function

--- src/app/view/utils.ts
+++ src/app/view/utils.ts
@@ -1,26 +1,16 @@
-/* eslint-disable operator-linebreak */
-/* eslint-disable implicit-arrow-linebreak */
-
 import { PER_MONTH, PER_YEAR } from '../const';
-import {
-  TAdditionalService, //
-  TCurrency,
-  TMembershipPlan,
-  TPlanData,
-} from '../data/types';
-
-export const formatCurrency = (currency: TCurrency) =>
-  new Intl.NumberFormat(undefined, {
-    style: 'currency',
-    currency,
-    maximumFractionDigits: 0,
-  });
+import { TAdditionalService, TMembershipPlan, TPlanData } from '../data/types';
 
 export const getItemPrice = (
   item: TMembershipPlan | TAdditionalService | number,
   data: TPlanData,
 ) => {
-  const fn = formatCurrency(data.currency);
+  const fn = new Intl.NumberFormat(undefined, {
+    style: 'currency',
+    currency: data.currency,
+    maximumFractionDigits: 0,
+  });
+
   if (typeof item === 'number') {
     return data.paymentPeriod === 'year'
       ? `${fn.format(item)}${PER_YEAR}`
